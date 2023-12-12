be8700546092989935343432c3a538e0163b3b17
Ulia <yliia192@gmail.com>
2023-12-12 11:36:47 +0300

    person

--- src/authorization.js
+++ src/authorization.js
@@ -9,7 +9,6 @@ async function authorizeAndFetchData() {
 		})
 		const page = await browser.newPage()
 
-
 		await page.goto('https://lardi-trans.com/log/mygruztrans/gruz/published/')
 		await page.waitForNavigation()
 		await page.type(
@@ -19,8 +18,13 @@ async function authorizeAndFetchData() {
 		await page.type('input[type="password"]', config.authorization.password)
 		await page.click('button[type="submit"]')
 
+		await page.waitForNavigation()
+		await page.waitForSelector('.proposal-table--column--contact')
+
+		const person = await page.$eval('.proposal-table--column--contact', (el) => el.textContent.trim());
+
 		await browser.close()
-		return 'title'
+		return person;
 	} catch (error) {
 		console.error('Помилка при отриманні даних:', error)
 		throw new Error(error.message)
